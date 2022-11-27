d04bb2041fcbcba1b0233a6ce0134ebbf72f758a
TatianaOsipovaT <tanechka.osipova.1993@mail.ru>
2022-11-27 11:40:41 +0400

    Dz lesson25

--- lesson25.py
+++ lesson25.py
@@ -2,22 +2,22 @@ from random import randint
 n = int(input('Введите число N - '))
 numbers = []
 for i in range(n):
-numbers.append(randint(-n, n+1))
+    numbers.append(randint(-n, n+1))
 print(numbers)
 
 f = open('file.txt', 'w')
 while True:
-s = input('Укажите позицию для вычисления - ')
-if s == "":
-break
+    s = input('Укажите позицию для вычисления - ')
+    if s == "":
+        break
 f.write(s+"\n")
 f.close()
 
 result = 1
 f = open('file.txt', 'r')
 for line in f:
-if line == "":
-break
+    if line == "":
+        break
 result *= numbers[int(line)]
 f.close()
 print(result)

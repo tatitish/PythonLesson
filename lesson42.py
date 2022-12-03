1c997d126eaf63be14daf95f0b5da67f25723c63
tatitish <109479383+tatitish@users.noreply.github.com>
2022-12-03 13:45:13 +0400

    Update lesson42.py

--- lesson42.py
+++ lesson42.py
@@ -12,3 +12,16 @@ while num > 1:
     else:
         d += 1
 print(my_list)
+
+# n = int(input("Введите число N: "))
+# i = 2
+# list = []
+
+# while i <= n:
+# if n % i == 0:
+# list.append(i)
+# n //= i
+# i = 2
+# else:
+# i += 1
+# print(f"Простые множители введенного числа указаны в списке: {list}")

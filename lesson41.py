4ef9111ec1ea45f0542fdced3002f0585086e2b1
tatitish <109479383+tatitish@users.noreply.github.com>
2022-12-03 13:37:12 +0400

    Update lesson41.py

--- lesson41.py
+++ lesson41.py
@@ -10,3 +10,14 @@ from math import pi
 
 d = input('Введите число d с заданной точностью: \n').count('0')
 print(f'Число π с заданной точностью {d} = {round(pi,d)}')
+
+# import math
+# d = input('Введите число d указывающее степень округления числа pi ')
+# d = d[2:len(d)]
+# print(round(math.pi,len(d)))
+
+# a = int(input('введите нужную точность 1#= '))
+# pi_target = 0
+# for i in range(1, 1000000):
+# pi_target += 4 * ((-1) ** (i + 1)) / (2 * i - 1)
+# print(str(pi_target)[:a + 2])

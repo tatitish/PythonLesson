89eb6ca111bc67f267ca70a6ac44679466e395a9
tatitish <109479383+tatitish@users.noreply.github.com>
2022-12-07 09:32:14 +0400

    Update lesson52.py

--- lesson52.py
+++ lesson52.py
@@ -121,3 +121,26 @@ mode_sel(mode_s)
 # print(f'Счет второго игрока: {score2}'
 
 
+
+
+# from random import randint
+
+# a = int(input('Введите количество конфет'))
+# hod = 0
+# wins = {0: 'Игрок', 1: 'Бот'}
+# while a > 0:
+# if a <= 28:
+# print(f'Выиграл {wins[hod]}')
+# break
+# if hod % 2 == 0:
+# print('Ход Игрока')
+# d = int(input('Введите количество конфет, которое хотите взять'))
+# a -= d
+# print(f'Осталось конфет: {a}')
+# else:
+# print('Ход Бота')
+# d = a % 29
+# a -= d
+# print(f'Осталось конфет: {a}')
+# hod = (hod + 1) % 2
+
